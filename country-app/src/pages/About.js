76d37ea7795edcb9bb21cf3c19adc0ad0c83e2d5
thomas <scozzari.thomas@yahoo.fr>
2024-04-12 14:43:01 +0200

    fin cours

---
+++ country-app/src/pages/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation></Navigation>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        ratione quam fugiat sint! Quidem, fuga incidunt? Consectetur quasi amet,
+        reprehenderit qui necessitatibus inventore quos doloremque recusandae
+        quo aliquid quia vel distinctio quaerat architecto quod totam, sed
+        deleniti dignissimos? Quibusdam, ratione alias! Veniam, cupiditate
+        sapiente quasi dolorum consequuntur incidunt tempora vel aliquid id
+        exercitationem totam blanditiis ipsa corrupti aperiam qui reprehenderit
+        quia natus quos explicabo nemo molestiae? Cum sit quia, quos expedita
+        culpa alias facilis enim, perspiciatis explicabo distinctio nihil
+        dolorum totam maiores iste voluptates nostrum earum iusto obcaecati vero
+        assumenda officia vel, ratione maxime. Odit quam odio molestiae?
+        Provident, amet.
+      </p>
+      <b />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui autem
+        possimus iusto voluptates impedit repellat. Omnis cupiditate dicta
+        officiis facere magnam aliquid nesciunt assumenda deserunt eveniet
+        magni, laborum saepe qui repellendus expedita excepturi, porro cumque
+        provident ad architecto sunt suscipit labore quos. Recusandae quaerat
+        exercitationem consectetur dolor aut aperiam natus debitis illum dolores
+        animi maxime, sit corporis laboriosam consequatur praesentium cupiditate
+        quod ipsa. Sint eveniet commodi expedita quas repellat sunt totam
+        labore. Maxime quos laudantium vero necessitatibus maiores, ratione odit
+        iste unde nulla aspernatur laboriosam tempore impedit quaerat quam in
+        natus. Pariatur reprehenderit perspiciatis officia tempora aliquid
+        repudiandae accusantium provident?
+      </p>
+    </div>
+  );
+};
+
+export default About;
